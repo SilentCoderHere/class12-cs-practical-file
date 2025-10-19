76bdf1ed521456593766edd535e63ccfbb1e2d54
itzpremsingh <premsingh@tuta.io>
2025-10-19 11:59:23 +0530

    Update 1 file: q16.py

---
+++ q16.py
@@ -0,0 +1,18 @@
+# Write a python program to connect to a database and create a table named ‘Students’ with attributes ‘Roll No, Name, and Class’.
+
+import mysql.connector as sql
+
+con = sql.connect(
+    host="localhost",
+    user="root",
+    password="pass1234",
+    database="School"
+)
+
+cur = con.cursor()
+
+cur.execute("CREATE TABLE Students(rollno INT, name VARCHAR(30), class INT)")
+
+con.commit()
+cur.close()
+con.close()
