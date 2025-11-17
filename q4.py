99c872e1474eee853a57926f2059b9100d741db3
Prem Singh <premsingh@tuta.io>
2025-11-18 00:04:51 +0530

    Update

--- q4.py
+++ q4.py
@@ -1,26 +1,31 @@
 # Write a python program to swap 2 numbers without using third variable and by using third variable through user defined functions.
 
+
 def swap_without_third():
+    print("Without third variable")
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
-    
+
     a = a + b
     b = a - b
     a = a - b
-    
+
     print("a:", a)
     print("b:", b)
-    
+
+
 def swap_with_third():
+    print("With third variable")
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
-    
+
     temp = a
     a = b
     b = temp
-    
+
     print("a:", a)
     print("b:", b)
-    
+
+
 swap_without_third()
 swap_with_third()
