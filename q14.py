810fe3890d0ae2b2c64af7aa41a6ef1ebc9cfbfd
Prem Singh <premsingh@tuta.io>
2025-11-18 00:08:35 +0530

    Update

--- q14.py
+++ q14.py
@@ -1,13 +1,13 @@
 # Write a python program to search for a specific word in a text file.
 
-word = "hello"
+word = input("Enter word: ")
 
 file = open("text.txt", "r")
 for line in file.readlines():
     if word in line:
         print("Found")
         break
-        
+
 else:
     print("Not found")
 
