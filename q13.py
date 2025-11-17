6cd61977fc8dc932e9d81fa48987de7e9eea337b
Prem Singh <premsingh@tuta.io>
2025-11-18 00:08:20 +0530

    Update

--- q13.py
+++ q13.py
@@ -2,21 +2,29 @@
 
 import csv
 
+
 def create_csv(filename, data):
-    file = open(filename, 'w', newline='')
+    file = open(filename, "w", newline="")
     writer = csv.writer(file)
     writer.writerows(data)
     file.close()
 
+
 def write_csv(filename, row):
-    file = open(filename, 'a', newline='')
+    file = open(filename, "a", newline="")
     writer = csv.writer(file)
     writer.writerow(row)
     file.close()
 
+
 def read_csv(filename):
-    file = open(filename, 'r')
+    file = open(filename, "r")
     reader = csv.reader(file)
     for row in reader:
         print(row)
     file.close()
+
+
+create_csv("data.csv", [["Name", "Age"], ["Alice", 20], ["Bob", 30]])
+write_csv("data.csv", ["Charlie", 40])
+read_csv("data.csv")
