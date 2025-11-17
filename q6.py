3ddd78851b819176624d25db7db5bd8b7daae06c
Prem Singh <premsingh@tuta.io>
2025-11-18 00:05:28 +0530

    Update

--- q6.py
+++ q6.py
@@ -1,10 +1,10 @@
 # 6. Write a python program to catch the IndexError exception in sequence data type(string, list, tuples).
 
 t = (1, 2, 3, 4)
-i = int(input("Enter idex: "))
+i = int(input("Enter index: "))
 try:
     value = t[i]
     print(value)
-        
+
 except IndexError:
     print("Out of range")
